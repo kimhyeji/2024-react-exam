9f38bf548e87d32b2a79a9afb275e081eaab9960
koreait <climax00@naver.com>
2024-02-05 21:15:51 +0900

    다양한 증감

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div>안녕</div>;
-}
+import React, { useState } from 'react';
 
-export default App;
+export default function App() {
+  const [num, setNum] = useState(0);
+
+  const onBtnIncreaseClicked = () => setNum(num + 1);
+  const onBtnDecreaseClicked = () => setNum(num - 1);
+
+  return (
+    <>
+      NUM : {num}
+      <br/>
+      <button onClick={onBtnIncreaseClicked}>+1</button>
+      <button onClick={onBtnDecreaseClicked}>-1</button>
+      <button onClick={() => setNum(num + 10)}>+10</button>
+      <button onClick={() => setNum(num - 10)}>-10</button>
+    </>
+  );
+}
