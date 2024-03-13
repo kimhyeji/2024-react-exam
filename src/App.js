3fd4a3c9c9547ed4db634e067c4fe2806f0df904
koreait <climax00@naver.com>
2024-03-13 20:32:40 +0900

    스톱워치 구현

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import React, { useState } from 'react';
 export default function App() {
   const [num, setNum] = useState(0);
 
-  const onBtnIncreaseClicked = () => setNum(num + 1);
-  const onBtnDecreaseClicked = () => setNum(num - 1);
+  const timeout = setTimeout(() => setNum(num + 1), 1000);
+
+  const pause = () => clearTimeout(timeout);
+
+  const resume = () => {
+    setTimeout(() => setNum(num + 1), 1000);
+  }
 
   return (
-    <>
-      NUM : {num}
-      <br/>
-      <button onClick={onBtnIncreaseClicked}>+1</button>
-      <button onClick={onBtnDecreaseClicked}>-1</button>
-      <button onClick={() => setNum(num + 10)}>+10</button>
-      <button onClick={() => setNum(num - 10)}>-10</button>
-    </>
+      <>
+        숫자 : {num}
+        <br />
+        <button onClick={pause}>일시정지</button>
+        <br />
+        <button onClick={resume}>재개</button>
+      </>
   );
 }
