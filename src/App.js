b0a454ed3956495aa49201db4e37a40cd0446157
koreait <climax00@naver.com>
2024-03-15 19:38:07 +0900

    간단한 팝업 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState('lee');
-  const [age, setAge] = useState(20);
-  const [address, setAddress] = useState('대전 둔산동');
+  const border = '10px solid red';
+  const [popupVisible, setPopupVisible] = useState(true);
 
   return <>
-    이름 : <input type="text" placeholder="이름을 입력해주세요" value={name}onChange={(e) => {
-      setName(e.target.value);
-    }}/>
-    <br />
-    나이 : <input type="number" placeholder="나이를 입력해주세요" value={age} onChange={(e) => {
-      setAge(e.target.valueAsNumber);
-    }}/>
-    <br />
-    주소 : <input type="text" placeholder="주소를 입력해주세요" value={address} onChange={(e) => {
-      setAddress(e.target.value);
-    }}/>
+    <button className='btn' onClick={() => setPopupVisible(!popupVisible)}>
+      팝업{popupVisible ? "닫기" : "열기"}
+    </button>
+    <div className='my-5 border'></div>
+    {popupVisible && <div style={{width: 100, height: 100, border}}></div>}
   </>
 }
