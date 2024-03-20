ee6cd9e39773773e49300bba7845e1f8d4003f39
koreait <climax00@naver.com>
2024-03-20 20:13:21 +0900

    useState, useRef 차이점

--- src/App.js
+++ src/App.js
@@ -1,60 +1,14 @@
 import React, { useState, useRef } from 'react';
 
 export default function App() {
-  const noInputRef = useRef(null);
-  const [no, setNo] = useState("");
+  const [no1, setNo1] = useState(0);
+  const no2Ref = useRef(0);
 
-  const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
-
-  const saveNo = () => {
-    if (no === "") {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setRecordedNos([...recordedNos, no]);
-    setNo("");
-    noInputRef.current.focus();
-  };
-
-  //const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
-  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          saveNo();
-        }}
-      >
-        <input
-          type="number"
-          value={no}
-          ref={noInputRef}
-          onChange={(e) => setNo(e.target.valueAsNumber)}
-        />
-        <button type="submit">기록</button>
-      </form>
-
-      <hr />
-
-      <h1>기록된 숫자 v1</h1>
-      {recordedNos.join(",")}
-
-      <hr />
-
-      <h1>기록된 숫자 v2</h1>
-      <ul>{li}</ul>
-
-      <hr />
-
-      <h1>기록된 숫자 v2-2</h1>
-      <ul>
-        {recordedNos.map((el, index) => (
-          <li key={index}>{el}</li>
-        ))}
-      </ul>
+    <button className="btn btn-primary" onClick={() => setNo1(no1 + 1)}>숫자 1 증가 : {no1}</button>
+    <button className="btn btn-primary" onClick={() => no2Ref.current++}>숫자 2 증가 : {no2Ref.current}</button>
     </>
   );
 }
