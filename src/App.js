fd5b52426fb8496fa47e962698ac3eda02583f8d
koreait <climax00@naver.com>
2024-03-20 19:48:16 +0900

    숫자 기록상자에 useRef 사용

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { useState, useRef } from 'react';
 
-export default function Main() {
+export default function App() {
+  const noInputRef = useRef(null);
   const [no, setNo] = useState("");
-  const formInputNoRef = useRef(null);
 
-  const notice = () => {
-    formInputNoRef.current.focus();
+  const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
 
-    if ( !no ) {
+  const saveNo = () => {
+    if (no === "") {
       alert("숫자를 입력해주세요.");
       return;
     }
 
-    alert(`당신이 입력한 숫자는 ${no}입니다.`);
-    setNo('');
-  }
-  return <>
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      notice();
-    }}>
-      <input className='input input-bordered' type="text" placeholder='숫자' ref={formInputNoRef}
-      value={no} onChange={(e) => setNo(e.target.value)}/>
-      <button className='btn btn-primary'>실행</button>
-    </form>
-  </>;
+    setRecordedNos([...recordedNos, no]);
+    setNo("");
+    noInputRef.current.focus();
+  };
+
+  //const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
+  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
+        <input
+          type="number"
+          value={no}
+          ref={noInputRef}
+          onChange={(e) => setNo(e.target.valueAsNumber)}
+        />
+        <button type="submit">기록</button>
+      </form>
+
+      <hr />
+
+      <h1>기록된 숫자 v1</h1>
+      {recordedNos.join(",")}
+
+      <hr />
+
+      <h1>기록된 숫자 v2</h1>
+      <ul>{li}</ul>
+
+      <hr />
+
+      <h1>기록된 숫자 v2-2</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
