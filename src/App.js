a2d6f9f6ec6fe6e85d6bf5dd896d9742938f498f
koreait <climax00@naver.com>
2024-03-25 21:45:01 +0900

    다크모드 구현

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-let AppCallCount = 0;
-let SubCallCount = 0;
-
-function Sub({appNo}) {
-  SubCallCount++;
-
-  const [no, setNo] = useState(0);
-  const [no2, setNo2] = useState(0);
-
-  useEffect(() => {
-    console.log('effect 1 : 한 번만 실행');
-  }, []);
-
-  useEffect(() => {
-    console.log('effect 2 : 부모(App)이 appNo가 바뀔 때 마다 실행');
-  }, [appNo]);
-
-  useEffect(() => {
-    console.log('effect 3 : 나(Sub)의 no가 바뀔 때 마다 실행');
-  }, [no]);
-
-  useEffect(() => {
-    console.log('effect 4 : appNo, no가 바뀔 때 마다 실행');
-  }, [appNo, no]);
-
-  useEffect(() => {
-    console.log('effect 5 : 매번 실행');
-  });
-
-  return (
-    <>
-      <div className='p-10 border border-blue-500'>
-        AppNo : {appNo}
-        <hr />
-        <button className='btn' onClick={() => setNo(no + 1)}>sub 증가 : {no}</button>
-        <button className='btn' onClick={() => setNo2(no2 + 1)}>sub2 증가 : {no2}</button>
-      </div>
-    </>
-  );
-}
 
 export default function App() {
-  AppCallCount++;
-  const [no, setNo] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const html = document.getElementsByTagName('html')[0];
+
+    if ( isDark ) {
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
+  }, [isDark]);
 
   return (
     <>
-      <div className='p-10 border border-red-500'>
-        <button className='btn' onClick={() => setNo(no + 1)}>app 증가 : {no}</button>  
-        <hr />
-        <Sub appNo={no}/>
-      </div>
+    <div className='btn'>
+        <button onClick={() => setIsDark(!isDark)}>테마토글</button>
+    </div>
+
+    <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium modi molestias harum, consequuntur magni sit illo quisquam atque! Iste corporis vitae dignissimos officiis saepe labore odio molestiae consequatur? Impedit, natus?
+    </div>
+
+    <h1 className="color-primary">
+        안녕
+        반가워
+    </h1>
     </>
   );
 }
