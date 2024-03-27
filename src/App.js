412e5150553c0312745d557a6aa2a799c107203b
koreait <climax00@naver.com>
2024-03-27 21:17:19 +0900

    라디오버튼 다루기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const fruits = ['사과', '바나나', '귤', '코코넛'];
-
-  const [selecteds, setSelecteds] = useState(new Array(fruits.length).fill(true));
-
-  const toggleFruitSelected = (index) => {
-    const newSelecteds = selecteds.map((el, _index) => _index == index ? !el : el);
-    setSelecteds(newSelecteds);
-  }
-
-  const selectedFruits = selecteds.map((el, index) => el ? fruits[index] : el).filter(el => el);
-
+  const [selectedGender, setSelectedGender] = useState('W');
+  
   return (
     <>
-    <ul>
-      {fruits.map((fruit, index) => (
-        <li key={index}>
-          <label>
-            <input checked={selecteds[index]} type="checkbox" onChange={() => toggleFruitSelected(index)} />
-            {fruit}
-          </label>
-        </li>
-      ))}
-    </ul>
-      <div>
-        선택 상태 : {selecteds.join(', ')}
-        <hr />
-        선택된 과일 : {selectedFruits.join(', ')}
-      </div>
+    <label>
+      <input type="radio" name="gender" onChange={() => setSelectedGender('M')} checked={selectedGender == "M"} />
+      남성
+    </label>
+    <label>
+      <input type="radio" name="gender" onChange={() => setSelectedGender('W')} checked={selectedGender == "W"} />
+      여성
+    </label>
     </>
   );
 }
