1a00d540b1d79f93d3f0ea0ac97bd226fbebaab8
koreait <climax00@naver.com>
2024-03-27 20:11:31 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,68 +1,16 @@
-import React, { useState, useMemo } from 'react';
-
-function isPrimeNumber(no) {
-  for ( let i = 2; i < no; i++ ) {
-      if ( i * i > no ) {
-          break;
-      }
-      
-      if ( no % i == 0 ) {
-          return false;
-      }
-  }
-  
-  return true;
-}
-
-function getPrimeNumbers(max) {
-  const primeNumbers = [];
-  
-  for ( let i = 2; i <= max; i++ ) {
-      if ( isPrimeNumber(i) ) {
-          primeNumbers.push(i);
-      }
-  }
-  return primeNumbers;
-}
-
-function getPrimeNumbersCount(max) {
-  return getPrimeNumbers(max).length;
-}
+import React, { useState, useCallback } from 'react';
 
 export default function App() {
-  const [inputNo, setInputNo] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const primeNumbersCount = useMemo(() => getPrimeNumbersCount(inputNo), [inputNo]);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const form = e.target;
-
-    form.number.value = form.number.value.trim();
-
-    if ( form.number.value.length == 0 ) {
-      alert("숫자를 입력해주세요");
-      form.number.focus();
-
-      return;
-    }
-
-    const number = form.number.valueAsNumber;
-    form.number.focus();
-
-    setInputNo(number);
-  }
-
+  const handleClick = useCallback(() => setCount(count + 1), [count]);
 
   return (
     <>
-    <form onSubmit={onSubmit}>
-      <input type="number" name="number" placeholder='숫자를 입력해주세요' defaultValue="0"/>
-      <input type="submit" className='btn'value="확인"/>
-      <hr />
-      <div>소수의 개수 : {primeNumbersCount}</div>
-    </form>
+    <div>
+      <span>클릭 횟수 : {count}</span>
+      <button onClick={handleClick}>클릭</button>
+    </div>
     </>
   );
 }
