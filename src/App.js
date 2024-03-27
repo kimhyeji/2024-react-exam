135c5c0a76053f2a9b0821a664dd0c648737f237
koreait <climax00@naver.com>
2024-03-27 20:03:43 +0900

    useMemo 예제

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 
+function isPrimeNumber(no) {
+  for ( let i = 2; i < no; i++ ) {
+      if ( i * i > no ) {
+          break;
+      }
+      
+      if ( no % i == 0 ) {
+          return false;
+      }
+  }
+  
+  return true;
+}
+
+function getPrimeNumbers(max) {
+  const primeNumbers = [];
+  
+  for ( let i = 2; i <= max; i++ ) {
+      if ( isPrimeNumber(i) ) {
+          primeNumbers.push(i);
+      }
+  }
+  return primeNumbers;
+}
+
+function getPrimeNumbersCount(max) {
+  return getPrimeNumbers(max).length;
+}
 
 export default function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [inputNo, setInputNo] = useState(0);
 
-  useEffect(() => {
-    const html = document.getElementsByTagName('html')[0];
+  const primeNumbersCount = useMemo(() => getPrimeNumbersCount(inputNo), [inputNo]);
 
-    if ( isDark ) {
-      html.classList.add('dark');
-    } else {
-      html.classList.remove('dark');
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    form.number.value = form.number.value.trim();
+
+    if ( form.number.value.length == 0 ) {
+      alert("숫자를 입력해주세요");
+      form.number.focus();
+
+      return;
     }
-  }, [isDark]);
+
+    const number = form.number.valueAsNumber;
+    form.number.focus();
+
+    setInputNo(number);
+  }
+
 
   return (
     <>
-    <div className='btn'>
-        <button onClick={() => setIsDark(!isDark)}>테마토글</button>
-    </div>
-
-    <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium modi molestias harum, consequuntur magni sit illo quisquam atque! Iste corporis vitae dignissimos officiis saepe labore odio molestiae consequatur? Impedit, natus?
-    </div>
-
-    <h1 className="color-primary">
-        안녕
-        반가워
-    </h1>
+    <form onSubmit={onSubmit}>
+      <input type="number" name="number" placeholder='숫자를 입력해주세요' defaultValue="0"/>
+      <input type="submit" className='btn'value="확인"/>
+      <hr />
+      <div>소수의 개수 : {primeNumbersCount}</div>
+    </form>
     </>
   );
 }
