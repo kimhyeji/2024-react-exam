3c781eb8da40e412fef460e5c2644206c916fc61
koreait <climax00@naver.com>
2024-04-01 21:16:27 +0900

    커스텀 훅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { useRef, useState } from 'react';
 
-export default function App() {
+function TodoApp({todosState}) {
+  const onAddBtnClick = () => {
+    todosState.addTodo("안녕");
+  }
+
+  const onRemoveBtnClick = () => {
+    todosState.removeTodo(1);
+  }
+
+  const onModifyBtnClick = () => {
+    todosState.modifyTodo(1, 'ㅎㅎ');
+  }
+
+  return (
+    <>
+      <button className='btn btn-info' onClick={onAddBtnClick}>추가</button>
+      <button className='btn btn-info' onClick={onRemoveBtnClick}>삭제</button>
+      <button className='btn btn-info' onClick={onModifyBtnClick}>수정</button>
+      <hr />
+      <ul>
+        {todosState.todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id} {todo.regDate} {todo.content}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function useTodosState() {
   const [todos, setTodos] = useState([]);
   const lastTodoIdRef = useRef(0);  
 
@@ -27,31 +57,20 @@ export default function App() {
     setTodos(newTodos);
   }
 
-  const onAddBtnClick = () => {
-    addTodo("안녕");
+  return {
+    todos,
+    addTodo,
+    removeTodo,
+    modifyTodo
   }
+}
 
-  const onRemoveBtnClick = () => {
-    removeTodo(1);
-  }
-
-  const onModifyBtnClick = () => {
-    modifyTodo(1, 'ㅎㅎ');
-  }
+export default function App() {
+  const todosState = useTodosState();
 
   return (
     <>
-      <button className='btn btn-info' onClick={onAddBtnClick}>추가</button>
-      <button className='btn btn-info' onClick={onRemoveBtnClick}>삭제</button>
-      <button className='btn btn-info' onClick={onModifyBtnClick}>수정</button>
-      <hr />
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo.id} {todo.regDate} {todo.content}
-          </li>
-        ))}
-      </ul>
+      <TodoApp todosState={todosState} />
     </>
   );
 }
