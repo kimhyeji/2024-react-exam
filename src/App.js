3de52facc6e4901b3586159f0c1d264040fcf408
koreait <climax00@naver.com>
2024-04-10 21:24:50 +0900

    할 일 리스트 옵션 드로어까지 작업 마무리

--- src/App.js
+++ src/App.js
@@ -1,113 +1,18 @@
-import React, { useRef, useState } from 'react';
-import { Button, Input } from '@mui/material';
+import React, { useRef, useState, useEffect, useMemo } from 'react';
+import { 
+  AppBar, 
+  Button, 
+  Chip, 
+  TextField, 
+  Toolbar, 
+  SwipeableDrawer, 
+  List, 
+  ListItem, 
+  ListItemButton, 
+  Divider
+} from '@mui/material';
+import classNames from 'classnames';
 
-function TodoListItem({todosState, todo, index}) {
-  const [editMode, setEditMode] = useState(false);
-  const [editedContent, setEditedContent] = useState(todo.content);
-  const editedContentInputRef = useRef(null);
-
-  const removeTodo = () => {
-    todosState.removeTodo(index);
-  }
-
-  const showEdit = () => {
-    setEditMode(true);
-  }
-
-  const commitEdit = () => {
-    if ( editedContent.trim().length == 0 ) {
-      alert('할 일을 입력해주세요');
-      editedContentInputRef.current.focus();
-      return;
-    }
-
-    todosState.modifyTodo(index, editedContent.trim());
-
-    setEditMode(false);
-  }
-
-  const cancelEdit = () => {
-    setEditMode(false);
-    setEditedContent(todo.content);
-  }
-
-  return (
-    <li>
-      {todo.id}
-      &nbsp;
-      {todo.regDate}
-      &nbsp;
-      {editMode || (
-        <>
-          {todo.content}
-          &nbsp;
-          <Button variant='outlined' onClick={showEdit}>수정</Button> 
-        </>
-      )}
-      {editMode && <>
-        <Input variant='plain' type="text" placeholder='할 일을 입력해주세요.' value={editedContent}
-        onChange={(e) => setEditedContent(e.target.value)}/>
-        &nbsp;
-        <Button variant='outlined' onClick={commitEdit}>수정완료</Button>
-        &nbsp;
-        <Button variant='outlined' onClick={cancelEdit}>수정취소</Button>
-      </>}
-      &nbsp;
-      <Button variant='outlined' onClick={removeTodo}>삭제</Button>
-    </li>
-  );
-}
-
-function TodoList({todosState}) {
-  return (
-    <ul>
-      {todosState.todos.map((todo, index) => (
-        <TodoListItem todosState={todosState} key={todo.id} todo={todo} index={index} />
-      ))}
-    </ul>
-  );
-}
-
-function NewTodoForm({todosState}) {
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const form = e.target;
-
-    form.content.value = form.content.value.trim();
-
-    if ( form.content.value.length == 0 ) {
-      alert('할 일을 입력해주세요');
-      form.content.focus();
-      return;
-    }
-
-    todosState.addTodo(form.content.value);
-    form.content.value = '';
-    form.content.focus();
-
-  }
-  return (
-    <form onSubmit={onSubmit}>
-      <Input type="text" variant='plain' name="content" autoComplete='off' placeholder='할 일을 입력해주세요'/>
-      &nbsp;
-      <Button type="submit" variant='outlined'>추가</Button>
-      &nbsp;
-      <Button type="reset" variant='outlined'>취소</Button>
-    </form>
-  );
-
-}
-
-function TodoApp({todosState}) {
-  return (
-    <>
-      <NewTodoForm todosState={todosState}/>
-      <hr />
-      <TodoList todosState={todosState}/>
-    </>
-  );
-}
 
 function useTodosState() {
   const [todos, setTodos] = useState([]);
@@ -122,7 +27,7 @@ function useTodosState() {
       content: newContent,
     }
 
-    const newTodos = [...todos, newTodo];
+    const newTodos = [newTodo, ...todos];
     setTodos(newTodos);
   }
 
@@ -144,12 +49,171 @@ function useTodosState() {
   }
 }
 
-export default function App() {
-  const todosState = useTodosState();
+function TodoListItem({todo, index, openDrawer}) {
+  return (
+    <>
+      <li key={todo.id} className='mt-10'>
+        <div className='flex gap-3'>
+          <Chip label={todo.id} variant="outlined" />
+          <Chip label={todo.regDate} color="primary" variant="outlined" />
+        </div>
+        <div className='mt-4 shadow rounded-[10px] flex'>
+          <Button className='flex-shrink-0 !rounded-[10px_0_0_10px]'>
+            <span
+            className={
+              classNames(
+                "text-2xl",
+                {
+                  "text-[color:var(--mui-color-primary-main)]":
+                  index % 2 == 0
+                },
+                { "text-[#dfdfdf]" : index % 2 != 0 }
+              )}
+            >
+              <i className="fa-solid fa-check"></i>
+            </span>
+          </Button>
+          <div className="flex-shrink-0 w-[2px] bg-[#dfdfdf] my-5"></div>
+          <div className='flex-grow whitespace-pre-wrap leading-relaxed hover:text-[color:var(--mui-color-primary-main)] flex items-center my-5 mx-3'>
+            {todo.content}
+          </div>
+          <Button
+            onClick={() => openDrawer(todo.id)}
+            className='flex-shrink-0 !rounded-[0_10px_10px_0]'
+          >
+            <span className='text-[#dfdfdf] text-2xl'>
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </span>
+          </Button>
+        </div>
+      </li>
+    </>
+  );
+}
+
+function useTodoOptionDrawerState() {
+  const [todoId, setTodoId] = useState(null);
+  const opened = useMemo(() => todoId !== null, [todoId]);
+  const close = () => setTodoId(null);
+  const open = (id) => setTodoId(id);
+
+  return {
+    todoId,
+    opened,
+    close,
+    open
+  };
+}
+
+function TodoOptionDrawer({state}) {
+  return(
+    <>
+      <SwipeableDrawer
+        anchor={"bottom"}
+        open={state.opened}
+        onClose={state.close}
+      >
+        <List className='!py-0'>
+          <ListItem className='!p-5'>
+            <span className="text-[color:var(--mui-color-primary-main)] font-bold">{state.todoId}번</span>
+          </ListItem>
+          <Divider variant="middle" />
+          <ListItemButton className='!p-5'>
+            <i class="fa-regular fa-pen-to-square"></i>
+            <span className='ml-1'>수정</span>
+            </ListItemButton>
+          <ListItemButton className='!p-5'>
+            <i class="fa-regular fa-trash-can"></i>
+            <span className='ml-1'>삭제</span>
+          </ListItemButton>
+        </List>
+      </SwipeableDrawer>
+    </>
+  );
+}
+
+function TodoList({todosState}) {
+  const todoOptionDrawerState = useTodoOptionDrawerState();
 
   return (
     <>
-      <TodoApp todosState={todosState} />
+      <TodoOptionDrawer state={todoOptionDrawerState} />
+      <div className='mt-4 px-4'>
+        <ul>
+          {todosState.todos.map((todo, index) => (
+            <TodoListItem 
+              key={todo.id}
+              todo={todo}
+              index={index}
+              openDrawer={todoOptionDrawerState.open}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function NewTodoForm({todosState}) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    form.content.value = form.content.value.trim();
+
+    if ( form.content.value.length == 0 ) {
+      alert('할 일을 입력해주세요');
+      form.content.focus();
+      return;
+    }
+
+    todosState.addTodo(form.content.value);
+    form.content.value = '';
+    form.content.focus();
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit} className='flex flex-col mt-4 px-4 gap-2'>
+        <TextField
+          minRows={3}
+          maxRows={10}
+          multiline
+          name="content"
+          autoComplete='off'
+          label='할 일을 입력해주세요'
+          variant='outlined'
+        />
+        <Button type="submit" variant='contained'>추가</Button>
+      </form>
+    </>
+  );
+}
+
+export default function App() {
+  const todosState = useTodosState();
+
+  useEffect(() => {
+    todosState.addTodo('운동');
+    todosState.addTodo('코딩');
+    todosState.addTodo('공부');
+  }, []);
+
+  return (
+    <>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <div className="flex-1"></div>
+          <div>TODO</div>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar/>
+
+      <NewTodoForm todosState={todosState} />
+
+      <TodoList todosState={todosState} />
     </>
   );
 }
